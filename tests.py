840b4949617a1e1b055b57b796912a8b23c17708
Joseph Cotton <jtcotton63@gmail.com>
2016-01-15 09:42:32 -0700

    Begun

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
-__author__ = 'Your Name'
-__email__ = 'Your Email'
+__author__ = 'Joseph Cotton'
+__email__ = 'jtcotton63@gmail.com'
 
 import unittest
 from unrolled_linked_list import UnrolledLinkedList
@@ -11,12 +11,33 @@ To run your tests, just run `python tests.py`
 '''
 
 
-class ExampleTest(unittest.TestCase):
+class Test(unittest.TestCase):
     """ Demonstrates how the unittest framework works """
 
     def test_example(self):
-        new_list = UnrolledLinkedList()
-        self.assertIsNotNone(new_list)
+        # new_list = UnrolledLinkedList()
+        # self.assertIsNotNone(new_list)
+        pass
+
+    def test_append(self):
+    	test_list = UnrolledLinkedList(3)
+    	test_list.append(0)
+    	test_list.append(1)
+    	test_list.append(2)
+    	test_list.append(3)
+    	test_list.append(4)
+    	test_list.append(5)
+    	assert 0 in test_list
+    	assert 1 in test_list
+    	assert 2 in test_list
+    	assert 3 in test_list
+    	assert 4 in test_list
+    	assert 5 in test_list
+    	assert 6 not in test_list
+    	assert str(test_list) == "{[0,1,2], [3,4,5]}"
+
+
 
 if __name__ == '__main__':
     unittest.main()
+
--- unrolled_linked_list.py
+++ unrolled_linked_list.py
@@ -1,26 +1,285 @@
-__author__ = 'Your Name'
-__email__ = 'Your Email'
+__author__ = 'Joseph Cotton'
+__email__ = 'jtcotton63@gmail.com'
 
+import math
 
 class UnrolledLinkedList(object):
-    """ This is the container class for your unrolled linked list """
-
-    class Node(object):
-        """ This is the node object you should use within your unrolled linked
-            list
-        """
-
-        def __init__(self):
-            pass
 
     def __init__(self, max_node_capacity=16):
-        """  The constructor for the list.
+        self.max_node_capacity = max_node_capacity
+        self.head = Node()
 
-        The default max node capacity is 16, but this value should be
-        overridable.
+
+
+
+    """ BASIC """
+
+    def append(self, data):
+        """ Add a new object to the end of the list.
+
+        This adds a new object, increasing the overall size of the list by 1.
+
+        RULES FOR APPENDING (paraphrased from Wikipedia):
+        To insert a new element, we simply find the node the element should be
+        in and insert the element into the elements array, incrementing
+        the size of the list. If the array is already full, we first insert a
+        new node either preceding or following the current one and move half of
+        the elements in the current node into it.
+
+        Usage: `my_list.append(4)`
+
+        Args:
+            data: The new object to be added to the list
+
+        Returns:
+            nothing
 
         """
-        pass
+        last_node = find_last_node()
+        if last_node.is_array_full():
+        	temp = Node()
+        	#TODO What happens when max_node_capacity is 1 or 2 ???
+        	if self.max_node_capacity == 1:
+        		temp.set_value_at_index(self, 0, data)
+        		#TODO Delete from old array
+    		elif self.max_node_capacity == 2:
+    			temp.set_value_at_index(self, 0, data)
+    			# DO NOT NEED TO DELETE FROM OLD ARRAY
+	        else:
+	        	middle = math.floor(len(last_node)/2)
+	        	temp.array.extend(last_node.array[middle:])
+	        	#TODO Delete from old array
+        	last_node.set_next_node(self, temp)
+    	else:
+    		last_node.array.append(self, data)
+
+
+    def __getitem__(self, index):
+        """ Access the element at the given index.
+
+        The indexes of an unrolled linked list refers to the total collection
+        of the list. i.e. in {[1, 2, 3], [5, 4, 1]}, index @ 1 refers to the
+        value 2. Index @ 4 refers to the value 4, even though it is in another
+        node.
+
+        This function should support negative indices, which are natural to
+        Python. For example, getting at index -1 should return the last
+        element, index -2 should be the second-to-last element and so on.
+
+        Usage: `my_list[4]`
+
+        BONUS: Allow this to work with slices. The resulting structure should
+        be a new balanced unrolled linked list.
+        For example,
+        my_list = {[1, 2, 6], [9, 6, 1], [0, 8, 1], [8, 2, 6]}
+        then my_list[4:10] should be {[6, 1, 0], [8, 1, 8, 2, 6]}
+
+        Args:
+            index: An int value indicating an index in the list.
+
+        Returns:
+            The object held at the given `index`.
+
+        Raises:
+            TypeError: If index is not an `int` object.
+            IndexError: If the index is out of bounds.
+
+        """
+        items_node, items_index_in_node_array = get_node_with_data_at_index(head, index)
+        return items_node.array[items_index_in_node_array]
+		
+
+
+        
+
+    def __setitem__(self, index, value):
+        """ Sets the item at the given index to a new value
+
+        Usage: `my_list[5] = "my new value"`
+
+        BONUS: Allow this to work with slices. You should *only* be able to
+        assign another unrolled linked list. Upon doing so, you should
+        rebalance the list. For example, if your node max capacity is 5, and
+        your list is:
+        my_list = {[1, 2, 6], [9, 6, 1], [0, 8, 1], [8, 2, 6]}
+        and you have another list with a different max capacity:
+        other_list = {[3, 6], [8, 4]},
+        and you use `my_list[0:2] = other_list` the result should be
+        {[3, 6, 8, 4, 6], [9, 6, 1], [0, 8, 1], [8, 2, 6]}
+        which is acceptable since the max capacity is 5. Node 0 did not go over
+
+        Args:
+            index: The index of the list which should be modified.
+            value: The new value for the list at the given index.
+
+        Returns:
+            none - this is a void function and should mutate the data structure
+                in-place.
+
+        Raises:
+            TypeError: If index is not an `int` object.
+            IndexError: If the index is out of bounds.
+        """
+        items_node, items_index_in_node_array = get_node_with_data_at_index(head, index)
+        items_node.set_value_at_index(self, items_index_in_node_array, value)
+
+    def __delitem__(self, index):
+        """ Deletes an item using the built-in `del` keyword
+
+        This function should support negative indices, which are natural to
+        Python. For example, deleting at index -1 should delete the last
+        element, index -2 should be the second-to-last element and so on.
+
+        RULES FOR DELETING (paraphrased from Wikipedia):
+        To remove an element, we simply find the node it is in and delete it
+        from the elements array, decrementing numElements. If this reduces the
+        node to less than half-full, then we move elements from the next_node
+        to fill it back up above half. If this leaves the next_node less than
+        half full, then we move all its remaining elements into the current
+        node, then bypass and delete it.
+
+        BONUS: Allow this to delete using slices as well as indices
+        (http://stackoverflow.com/questions/12986410/how-to-implement-delitem-to-handle-all-possible-slice-scenarios)
+
+        Usage: `del my_list[4]`
+
+        Args:
+            index: An `int` value indicating the index of the item you are
+                deleting.
+
+        Returns:
+            none - this is a void function that should mutate the data
+                structure in-place, not return a new data structure.
+
+        Raises:
+            TypeError: If index is not an `int` object.
+            IndexError: If the index is out of bounds.
+        """
+        
+
+
+
+
+
+
+
+    """ HELPER """
+
+    def find_last_node(self):
+    	if not hasattr(head, 'next_node'): # Make sure this syntax is correct
+    		return head
+		return find_last_node_recur(head)
+
+	def find_last_node_recur(curr_node):
+		if not hasattr(curr_node, 'next_node'):
+			return curr_node
+		return find_last_node_recur(curr_node.next_node)
+		pass
+
+	def get_node_with_data_at_index(curr_node, index):
+		""" This function finds the node with the data at the specified index relative
+			the entire list
+		
+			Ex: Calling get_node_with_data_at_index(head, 3) on this example array 
+			{[1,2,3],[5,4,1]} returns a pointer to the second node and the number 0
+			(the index of the desired item in the array of the node).
+		
+			Throws an IndexError if an item with the given index doesn't exist within
+			the list """
+		has_next_node = hasattr(curr_node, 'next_node')
+		if len(curr_node) > index and has_next_node:
+			return curr_node, index
+		elif has_next_node == True:
+			return get_node_with_data_at_index(curr_node.next_node, index - len(curr_node))
+		raise IndexError('No item in the list has the following index: ' + index)
+
+
+
+
+
+
+
+
+    """ EXTENDED """
+
+    def __contains__(self, item):
+        """ Returns True/False whether the list contains the given item
+
+        Usage: `5 in my_list`
+
+        Args:
+            item: The object for which containment is being checked for.
+
+        Returns:
+            True: if `item` is found somewhere in the list
+            False: if `item` is not found anywhere in the list
+        """
+        return contains_recur(self.head, item)
+
+	def contains_recur(curr_node, item):
+		if item in curr_node.array:
+			return True
+		elif hasattr(curr_node, 'next_node'):
+			return contains_recur(curr_node.next_node, item)
+		else: #Has reached end of linked list but hasn't found the element
+			return False
+
+	def __len__(self):
+		"""Return the total number of items in the list
+
+		Usage: `len(my_list)`
+
+		Returns:
+			An int object indicating the *total* number of items in the list,
+			NOT the number of nodes."""
+
+        count_num_elements_recur(self.head, 0)
+
+	def count_num_elements_recur(curr_node, count):
+		curr_node_size = len(curr_node.array)
+		if not hasattr(curr_node, 'next_node'):
+			return count + curr_node_size
+		return count_num_elements_recur(curr_node.next_node, count + curr_node_size)
+        
+
+	def __str__(self):
+		""" Returns a string representation of the list.
+
+		The format for representing an unrolled linked list will be as follows:
+			- curly braces indicates an unrolled linked list
+			- square brackets indicates a node
+			- all values are separated by a comma and a space
+		For example:
+		{[1, 2, 3], [0, 9, 8], [2, 4, 6]}
+		This list has three nodes and each node as three int objects in it.
+
+		Usage: `str(my_list)`
+
+		Returns:
+			A string representation of the list."""
+
+        string = '{'
+        result = string_elements_recur(self.head, string)
+        return result + '}'
+
+    def string_elements_recur(curr_node, string):
+		string += '['
+		i = 0
+		while i < len(curr_node.array):
+			string += data
+			if i != len(curr_node.array) - 1:
+				string += ','
+			i += 1
+		string += ']'
+		if hasattr(curr_node, 'next_node'):
+			string += ', '
+			return string_elements_recur(curr_node.next_node, string)
+
+
+
+
+
+    """ OPERATORS """
 
     def __add__(self, other):
         """ Appends two Unrolled Linked Lists end-to-end using `+`
@@ -64,112 +323,11 @@ class UnrolledLinkedList(object):
         """
         pass
 
-    def __getitem__(self, index):
-        """ Access the element at the given index.
 
-        The indexes of an unrolled linked list refers to the total collection
-        of the list. i.e. in {[1, 2, 3], [5, 4, 1]}, index @ 1 refers to the
-        value 2. Index @ 4 refers to the value 4, even though it is in another
-        node.
 
-        This function should support negative indices, which are natural to
-        Python. For example, getting at index -1 should return the last
-        element, index -2 should be the second-to-last element and so on.
 
-        Usage: `my_list[4]`
 
-        BONUS: Allow this to work with slices. The resulting structure should
-        be a new balanced unrolled linked list.
-        For example,
-        my_list = {[1, 2, 6], [9, 6, 1], [0, 8, 1], [8, 2, 6]}
-        then my_list[4:10] should be {[6, 1, 0], [8, 1, 8, 2, 6]}
-
-        Args:
-            index: An int value indicating an index in the list.
-
-        Returns:
-            The object held at the given `index`.
-
-        Raises:
-            TypeError: If index is not an `int` object.
-            IndexError: If the index is out of bounds.
-
-        """
-        pass
-
-    def __len__(self):
-        """Return the total number of items in the list
-
-        Usage: `len(my_list)`
-
-        Returns:
-            An int object indicating the *total* number of items in the list,
-            NOT the number of nodes.
-        """
-        pass
-
-    def __setitem__(self, index, value):
-        """ Sets the item at the given index to a new value
-
-        Usage: `my_list[5] = "my new value"`
-
-        BONUS: Allow this to work with slices. You should *only* be able to
-        assign another unrolled linked list. Upon doing so, you should
-        rebalance the list. For example, if your node max capacity is 5, and
-        your list is:
-        my_list = {[1, 2, 6], [9, 6, 1], [0, 8, 1], [8, 2, 6]}
-        and you have another list with a different max capacity:
-        other_list = {[3, 6], [8, 4]},
-        and you use `my_list[0:2] = other_list` the result should be
-        {[3, 6, 8, 4, 6], [9, 6, 1], [0, 8, 1], [8, 2, 6]}
-        which is acceptable since the max capacity is 5. Node 0 did not go over
-
-        Args:
-            index: The index of the list which should be modified.
-            value: The new value for the list at the given index.
-
-        Returns:
-            none - this is a void function and should mutate the data structure
-                in-place.
-
-        Raises:
-            TypeError: If index is not an `int` object.
-            IndexError: If the index is out of bounds.
-        """
-
-    def __delitem__(self, index):
-        """ Deletes an item using the built-in `del` keyword
-
-        This function should support negative indices, which are natural to
-        Python. For example, deleting at index -1 should delete the last
-        element, index -2 should be the second-to-last element and so on.
-
-        RULES FOR DELETING (paraphrased from Wikipedia):
-        To remove an element, we simply find the node it is in and delete it
-        from the elements array, decrementing numElements. If this reduces the
-        node to less than half-full, then we move elements from the next node
-        to fill it back up above half. If this leaves the next node less than
-        half full, then we move all its remaining elements into the current
-        node, then bypass and delete it.
-
-        BONUS: Allow this to delete using slices as well as indices
-        (http://stackoverflow.com/questions/12986410/how-to-implement-delitem-to-handle-all-possible-slice-scenarios)
-
-        Usage: `del my_list[4]`
-
-        Args:
-            index: An `int` value indicating the index of the item you are
-                deleting.
-
-        Returns:
-            none - this is a void function that should mutate the data
-                structure in-place, not return a new data structure.
-
-        Raises:
-            TypeError: If index is not an `int` object.
-            IndexError: If the index is out of bounds.
-        """
-        pass
+    """ ITERATORS """
 
     def __iter__(self):
         """ Returns an iterable to allow one to iterate the list.
@@ -184,46 +342,6 @@ class UnrolledLinkedList(object):
         """
         pass
 
-    def __contains__(self, item):
-        """ Returns True/False whether the list contains the given item
-
-        Usage: `5 in my_list`
-
-        Args:
-            item: The object for which containment is being checked for.
-
-        Returns:
-            True: if `item` is found somewhere in the list
-            False: if `item` is not found anywhere in the list
-        """
-        pass
-
-    def append(self, data):
-        """ Add a new object to the end of the list.
-
-        This adds a new object, increasing the overall size of the list by 1.
-
-        RULES FOR APPENDING (paraphrased from Wikipedia):
-        To insert a new element, we simply find the node the element should be
-        in and insert the element into the elements array, incrementing
-        the size of the list. If the array is already full, we first insert a
-        new node either preceding or following the current one and move half of
-        the elements in the current node into it.
-
-        For appending you should always create a new node at the end of the
-        list.
-
-        Usage: `my_list.append(4)`
-
-        Args:
-            data: The new object to be added to the list
-
-        Returns:
-            nothing
-
-        """
-        pass
-
     def __reversed__(self):
         """ Works just like __iter__, but starts from the back.
 
@@ -234,20 +352,39 @@ class UnrolledLinkedList(object):
         """
         pass
 
-    def __str__(self):
-        """ Returns a string representation of the list.
 
-        The format for representing an unrolled linked list will be as follows:
-            - curly braces indicates an unrolled linked list
-            - square brackets indicates a node
-            - all values are separated by a comma and a space
-        For example:
-        {[1, 2, 3], [0, 9, 8], [2, 4, 6]}
-        This list has three nodes and each node as three int objects in it.
 
-        Usage: `str(my_list)`
 
-        Returns:
-            A string representation of the list.
-        """
-        pass
+
+
+    """ NODE CLASS """
+
+    class Node(object):
+        def __init__(self):
+        	self.array = []
+
+        def get_next_node(self):
+        	return self.next_node
+
+        def set_next_node(self, next_node):
+        	self.next_node = next_node
+
+        def get_value_at_index(self, index):
+        	return array[index]
+
+    	def set_value_at_index(self, index, value):
+    		array[index] = value
+
+		def __len__(self):
+			return len(array)
+
+		def __contains__(self, value):
+			if value in self.array:
+				return True
+			else:
+				return False
+
+		def is_array_full(self):
+			return len(self.array) == max_node_capacity
+
+
